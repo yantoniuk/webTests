7031182ba764c79b8af8c317efefcc73a3852e63
Yana Antoniuk <yantoniuk@Yanas-MacBook-Air.local>
2021-02-02 14:04:24 +0300

    path of info fields was moved to constant

--- src/main/java/com/solvd/carina/demo/gui/pages/demoqa/TextBoxPage.java
+++ src/main/java/com/solvd/carina/demo/gui/pages/demoqa/TextBoxPage.java
@@ -2,6 +2,7 @@ package com.solvd.carina.demo.gui.pages.demoqa;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
+import com.solvd.carina.demo.gui.pages.demoqa.constant.TextBoxConstant;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
@@ -60,7 +61,7 @@ public class TextBoxPage extends AbstractPage {
     public String getInfo() {
         StringBuffer infoBuffer = new StringBuffer();
         List<ExtendedWebElement> infoFields =
-                findExtendedWebElements(By.xpath("//div[@class='border col-md-12 col-sm-12']/p[@id]"));
+                findExtendedWebElements(TextBoxConstant.RESULT_INFO_FIELDS);
         infoFields.stream().map(ExtendedWebElement::getText).forEach(el -> infoBuffer.append(el + "\n"));
         return infoBuffer.toString();
     }
